bd3a261257ef438fbd575d4204cd49d718b123ef
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-09-25 06:55:27 -0400

    Animated PageView

--- lib/widgets/posts_carousel.dart
+++ lib/widgets/posts_carousel.dart
@@ -3,124 +3,141 @@ import 'package:flutter/widgets.dart';
 import 'package:social_media_app/models/post_model.dart';
 
 class PostsCarousel extends StatelessWidget {
-  final PageController? pageController;
+  final PageController pageController;
   final String? title;
   final List<Post>? posts;
-  const PostsCarousel({Key? key, this.pageController, this.title, this.posts})
+  const PostsCarousel(
+      {Key? key, required this.pageController, this.title, this.posts})
       : super(key: key);
 
   _buildPost(BuildContext context, int index) {
     Post post = posts![index];
-    return Stack(
-      children: [
-        Container(
-          margin: EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                offset: Offset(0, 2),
-                blurRadius: 6.0,
-              )
-            ],
+    return AnimatedBuilder(
+      animation: pageController,
+      builder: (BuildContext context, widget) {
+        double value = 1;
+        if (pageController.position.haveDimensions) {
+          value = (pageController.page! - index);
+          value = (1 - (value.abs() * 0.25).clamp(0.0, 1.0));
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 400.0,
+            child: widget,
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Image(
-              height: 400.0,
-              width: 300.0,
-              fit: BoxFit.cover,
-              image: AssetImage(
-                post.imageUrl.toString(),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 10.0,
-          left: 10.0,
-          right: 10.0,
-          child: Container(
-            padding: EdgeInsets.all(12.0),
-            height: 110.0,
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(15.0),
-                bottomRight: Radius.circular(15.0),
-              ),
-              color: Colors.white54,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  post.title.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24.0,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(
-                  height: 6.0,
-                ),
-                Text(
-                  post.location.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18.0,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(
-                  height: 6.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ),
-                        Text(
-                          post.likes.toString(),
-                          style: TextStyle(
-                            fontSize: 18.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 6.0,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.comment,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        SizedBox(
-                          width: 6.0,
-                        ),
-                        Text(
-                          post.comments.toString(),
-                          style: TextStyle(
-                            fontSize: 18.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  offset: Offset(0, 2),
+                  blurRadius: 6.0,
                 )
               ],
             ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                height: 400.0,
+                width: 300.0,
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  post.imageUrl.toString(),
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 10.0,
+            left: 10.0,
+            right: 10.0,
+            child: Container(
+              padding: EdgeInsets.all(12.0),
+              height: 110.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+                color: Colors.white54,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    post.title.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 6.0,
+                  ),
+                  Text(
+                    post.location.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 6.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          Text(
+                            post.likes.toString(),
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 6.0,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.comment,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          SizedBox(
+                            width: 6.0,
+                          ),
+                          Text(
+                            post.comments.toString(),
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
