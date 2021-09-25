b41868e3217791b1e4d4204722b9a9a24e5d067e
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-09-25 11:46:44 -0400

    Finishing Our Profile Screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:social_media_app/models/user_model.dart';
 import 'package:social_media_app/widgets/custom_drawer.dart';
+import 'package:social_media_app/widgets/posts_carousel.dart';
 import 'package:social_media_app/widgets/profile_clipper.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -13,6 +15,17 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  late PageController _yourPostsPageController;
+  late PageController _yourFavoritesPageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _yourPostsPageController =
+        PageController(initialPage: 0, viewportFraction: 0.8);
+    _yourFavoritesPageController =
+        PageController(initialPage: 0, viewportFraction: 0.8);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +48,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
               top: 50.0,
               left: 20.0,
               child: IconButton(
-                onPressed: _scaffoldKey.currentState!.openDrawer(),
+                onPressed: () {},
+                // _scaffoldKey.currentState!.openDrawer(),
                 icon: Icon(Icons.menu),
                 color: Theme.of(context).primaryColor,
               ),
@@ -61,7 +75,74 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ),
             ),
-          ])
+          ]),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              widget.user!.name.toString(),
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Following',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 22.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  Text(
+                    widget.user!.following.toString(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Followers',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 22.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  Text(
+                    widget.user!.followers.toString(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          PostsCarousel(
+            pageController: _yourPostsPageController,
+            title: 'Your Post',
+            posts: widget.user!.posts,
+          ),
+          PostsCarousel(
+            pageController: _yourFavoritesPageController,
+            title: 'Favorites',
+            posts: widget.user!.favorites,
+          ),
+          SizedBox(
+            height: 50.0,
+          )
         ]),
       ),
     );
