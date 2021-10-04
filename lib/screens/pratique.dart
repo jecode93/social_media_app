4809439e8fda185d1f2ec8cbe299a50898f6d7eb
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-10-04 12:34:42 -0400

    Ajout d'un fichier nommer pratique

---
+++ lib/screens/pratique.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pratique extends StatelessWidget {
+  const Pratique({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
